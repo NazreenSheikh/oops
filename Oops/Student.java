4d9048274146c21af8ebcdd4672177755b048334
nazreen <nazreenshaikh0077@gmail.com>
2021-12-05 20:50:06 +0530

    Student class

--- Oops/Student.java
+++ Oops/Student.java
@@ -1,2 +1,30 @@
-package Oops;public class Student {
+package Oops;
+
+public class
+Student {
+        String name;
+        int rno;
+        static int totalStudent;
+
+        Student(){
+//            this.name= "noName";
+//            this.rno= 0;
+            this("noName",0);
+
+        }
+        Student(String naam, int roll){
+            this.name = naam;
+            this.rno= roll;
+            Student.totalStudent ++;
+//
+
+        }
+        void greeting(String name){
+            System.out.println("Hello " +this.name + " and " +name);
+            fun();
+        }
+        static void fun(){
+            System.out.println("hii I'm static fun");
+//            Greeting("naaz");  gives an error as we cannot call non-static function in static one
+        }
 }
